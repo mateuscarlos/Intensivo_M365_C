402b4e93679d172a1f5c611b11adc7afa5863050
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-10-29 08:56:46 -0300

    feat: update configuration files for deployment and add TypeScript build options

--- cs-learning-hub/vite.config.ts
+++ cs-learning-hub/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Intensivo_M365_C/",
   plugins: [react()],
   build: {
     outDir: "dist",
